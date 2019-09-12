49baf364de7f47135ee79265e28bec48d4a0ef9d
Victor Zhuravlev <mocambe@mail.ru>
2019-09-12 23:33:02 +0300

    Zhuravlev 12.09.2019. Edit element on double click added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const statefulElements = document.getElementsByClassName('list__item_text');
-const statefulArray = [].slice.call(statefulElements);
-const statefulIdArray = statefulArray.map(element => element.parentNode.id);
+const statefulElementsArray = [].slice.call(statefulElements);
+const statefulIdArray = statefulElementsArray.map(element => element.parentNode.id);
+const editableElements = document.getElementsByClassName('editable');
+const editableElementsArray = [].slice.call(editableElements);
 
 const maxId = statefulIdArray.reduce((acc, current) => current > acc ? current: acc, 0);
 
@@ -27,11 +29,25 @@ const setActiveElement = element => {
   element.classList.toggle('active');
 }
 
-const elementOnClick = element => {
+const handleOnClickElement = element => {
   setActiveElement(element);
   foldElement(element);
 }
 
+const makeEditable = element => {
+  element.contentEditable = true;
+  element.style.cursor = 'auto';
+  element.addEventListener('blur', event => {
+    event.target.contentEditable = false
+    event.target.style.cursor = 'pointer';
+  });
+}
 
-statefulArray.forEach(element => element.addEventListener('click', event => elementOnClick(event.target)));
 
+
+const setupSidebarNavigation = () => statefulElementsArray.forEach(element => element.addEventListener('click', event => event.detail === 1? handleOnClickElement(event.target): false));
+
+const setupEditableElements = () => editableElementsArray.forEach(element => element.addEventListener('dblclick', event => makeEditable(event.target)));
+
+setupSidebarNavigation();
+setupEditableElements();
