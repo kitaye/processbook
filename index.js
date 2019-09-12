ef5c1024b1193a5b8a512f1862b972f4f7779492
Victor Zhuravlev <mocambe@mail.ru>
2019-09-12 20:18:00 +0300

    Zhuravlev 12.09.2019. Unique id generator added.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 const statefulElements = document.getElementsByClassName('list__item_text');
 const statefulArray = [].slice.call(statefulElements);
+const statefulIdArray = statefulArray.map(element => element.parentNode.id);
+
+const maxId = statefulIdArray.reduce((acc, current) => current > acc ? current: acc, 0);
+
+const makeCounter = () => {
+  let counter = maxId + 1;
+  return () => counter += 1
+}
+
+const getUniqueId = makeCounter();
+
 
 const foldElement = element => {
   if(element.parentNode.childElementCount === 1) {
