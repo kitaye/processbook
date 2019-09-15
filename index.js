d61c0fd0350f3670447280af4a6131f227d17258
Victor Zhuravlev <mocambe@mail.ru>
2019-09-15 11:31:14 +0300

    Журавлев 14.05.2019. Рефакторинг функции удаления элементов

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const editableElementsArray = [... editableElements];
 const buttons = document.getElementsByClassName('button');
 const buttonsDeleteArray = [... buttons].filter(element => element.classList.contains('button-delete'));
 const buttonsAddArray = [... buttons].filter(element => element.classList.contains('button-add'));
-const buttonEditArray = [... buttons].filter(element => element.classList.contains('button-edit'));
+const buttonsEditArray = [... buttons].filter(element => element.classList.contains('button-edit'));
+const siblingsArray = element => [... element.parentNode.children];
 
 const maxId = statefulIdArray.reduce((acc, current) => current > acc ? current: acc, 0);
 
@@ -25,10 +26,19 @@ const foldElement = element => {
   element.parentNode.lastElementChild.classList.toggle('visually-hidden');
 };
 
+const toggleButtonsVisibilityForElement = element => {
+  const buttons = [... element.parentNode.children].filter(element => element.classList.contains('button'));
+  const isActive = element.classList.contains('active');
+  buttons.forEach(button => isActive ? button.classList.remove('visually-hidden'): button.classList.add('visually-hidden'));
+}
+
 const setActiveElement = element => {
   const activeElement = document.querySelector('.active');
-  activeElement.classList.toggle('active');
+  
+  activeElement && activeElement.classList.toggle('active');
   element.classList.toggle('active');
+  toggleButtonsVisibilityForElement(element);
+  activeElement && toggleButtonsVisibilityForElement(activeElement);
 }
 
 const setupSidebarNavigation = () => statefulElementsArray.forEach(element => element.addEventListener('click', event => event.detail === 1? handleOnClickElement(event.target): makeEditable(event.target)));
@@ -39,20 +49,43 @@ const handleOnClickElement = element => {
   foldElement(element);
 }
 
-const handleMouseOverElement = element => {
-  const siblingsArray = [... element.parentNode.children];
-  const buttonsArray = siblingsArray.filter(element => element.classList.contains('button'));
-  buttonsArray.forEach(element => element.classList.remove('visually-hidden'));
-  console.log('BUTTONS: ' + buttonsArray);
-  element.addEventListener('mouseout', () => buttonsArray.forEach(element => element.classList.add('visually-hidden')));
-}
+// const handleMouseOverElement = element => {
+//   const buttonsArray = siblingsArray(element).filter(element => element.classList.contains('button'));
+//   buttonsArray.forEach(element => element.classList.remove('visually-hidden'));
+//   console.log('BUTTONS: ' + buttonsArray);
+//   element.addEventListener('mouseout', () => buttonsArray.forEach(element => element.classList.add('visually-hidden')));
+// }
 
-statefulElementsArray.forEach(element => element.addEventListener('mouseover', event => handleMouseOverElement(event.target)));
+// statefulElementsArray.forEach(element => element.addEventListener('mouseover', event => handleMouseOverElement(event.target)));
 
 const deleteSidebarElement = element => {
   console.log(element.parentNode);
   element.parentNode.remove();
 }
 
-buttonsDeleteArray.forEach(element => element.addEventListener('click', event => deleteSidebarElement(event.target)));
+const makeElementEditable = element => {
+  const setEditPossibility = isPossible => {
+    if(isPossible === true) {
+      element.contentEditable = true;
+      element.focus();
+    }
+    element.contentEditable = 'false';
+    // element.style.cursor = element.classList.contains('list__item_text') ? 'pointer': 'auto';
+  }
+  
+  setEditPossibility(true);
+  element.addEventListener('blur', () => setEditPossibility('false'));
+}
+
+const makeElementsEditableOnButtonClick = buttons => {
+  const editableElement = button => siblingsArray(button).find(element => element.classList.contains('editable'));
+  buttons.forEach(button => button.onclick = event => {
+    console.log('EDITABLE ELEMENT: ' + editableElement(event.target));
+    makeElementEditable(editableElement(event.target))
+  });
+}
+
+console.log(buttonsEditArray);
+makeElementsEditableOnButtonClick(buttonsEditArray);
+buttonsDeleteArray.forEach(button => button.onclick = () => deleteSidebarElement(event.target));
 
